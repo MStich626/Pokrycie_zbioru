114d0a0de4631158fcc91b2f3fa03704a164e230
MStich626 <m.lukasik.ia16@wp.pl>
2023-08-25 17:07:34 +0200

    Problem z tablicą

--- pokrycie_zbioru.py
+++ pokrycie_zbioru.py
@@ -1,4 +1,5 @@
 from numpy import matrix
+import numpy as np
 """
 # Creating array step I
 price_vector_temp = input("Enter price vector (use space to separate): ")
@@ -15,10 +16,17 @@ for i in range(rows):
     arr.append(row)
 print(arr)
 """
+suma = []
+wartosci = []
+price = [20,60,40,50]
 arr = [[1,0,0,1],[1,1,1,0],[1,0,1,0],[0,1,0,1]]
+for i in range(0, len(arr)):
+    suma.append(sum(arr[i]))
+    wartosci.append(price[i] / suma[i])
 
-
-
+#print(wartosci[wartosci != 0])
+#x1 = np.min(wartosci[np.nonzero(wartosci)])
+"""
 def algorythm(price,services):
     wydatek = 0
     firmy = []
@@ -28,21 +36,18 @@ def algorythm(price,services):
     l_uslug = len(services)
     l_firm = l_uslug
 
+    while(True):
+        for i in range(0, len(services)):
+            suma.append(sum(services[i]))
+            wartosci.append(price[i] / suma[i])
 
-while(True):
-    for i in range(0, len(services)):
-        suma.append(sum(services[i]))
-        wartosci.append(price[i] / suma[i])
+        if sum(suma) == 0:
+            break
 
-    if sum(suma) == 0:
-        break
-
-    else:
-        x1 < - which(wartosci == min(wartosci[wartosci != 0]))
-        if (length(x1) > 1)
-            {
-                x1 < - x1[sample(1:length(x1), 1)]
-            }
+        else:
+            x1 = np.min(wartosci[np.nonzero(wartosci)])
+            if len(x1) > 1:
+                x1 = x1[random(0:len(x1), 1)]
             wspl < - which(uslugi[, x1] == 1 )
             uslugi[wspl,] < - 0
             suma[x1] < - 0
