ea4648f70f032a3765186386fce1a14c8ede432d
MStich626 <m.lukasik.ia16@wp.pl>
2023-08-25 16:41:03 +0200

    Pokrycie zbioru v.0.1

---
+++ pokrycie_zbioru.py
@@ -0,0 +1,57 @@
+from numpy import matrix
+"""
+# Creating array step I
+price_vector_temp = input("Enter price vector (use space to separate): ")
+price_vector = price_vector_temp.split( )
+rows = int(input("Enter number of rows:"))
+cols = rows
+arr = []
+
+# Creating array step II
+for i in range(rows):
+    row_input = input(f"Enter data for first row {i+1} (use space to separate): ")
+    row_values = row_input.split( )
+    row = [int(value) for value in row_values]
+    arr.append(row)
+print(arr)
+"""
+arr = [[1,0,0,1],[1,1,1,0],[1,0,1,0],[0,1,0,1]]
+
+
+
+def algorythm(price,services):
+    wydatek = 0
+    firmy = []
+    j = 1
+    suma = []
+    wartosci = []
+    l_uslug = len(services)
+    l_firm = l_uslug
+
+
+while(True):
+    for i in range(0, len(services)):
+        suma.append(sum(services[i]))
+        wartosci.append(price[i] / suma[i])
+
+    if sum(suma) == 0:
+        break
+
+    else:
+        x1 < - which(wartosci == min(wartosci[wartosci != 0]))
+        if (length(x1) > 1)
+            {
+                x1 < - x1[sample(1:length(x1), 1)]
+            }
+            wspl < - which(uslugi[, x1] == 1 )
+            uslugi[wspl,] < - 0
+            suma[x1] < - 0
+            wartosci[x1] < - 0
+            uslugi[, x1] < - 0
+            wydatek < - wydatek + cena[x1]
+            firmy[j] < - x1
+            j = j + 1
+
+
+algorythm(arr)
+"""
